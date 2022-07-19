f63e7709adca7626d968ba3bce641a3a894a0f3c
catalinamarina0 <102092703+catalinamarina0@users.noreply.github.com>
2022-07-19 21:25:10 +0200

    Moved testdata

--- Play.py
+++ Play.py
@@ -4,98 +4,8 @@ import ShowSolution
 
 def Config():
     printAll = 0
-    tubes = TestTubes2_1()
+    tubes = TestTubes4_1()
     return printAll,tubes
-
-def TestTubes1_1():     #10 turns
-    t1 = "ryby"
-    t2 = "rrbb"
-    t3 = "bryy"
-    t4 = ""
-    t5 = ""
-    return t1,t2,t3,t4,t5
-
-def TestTubes2_1():     #17 turns
-    t1 = "grrb"
-    t2 = "rypp"
-    t3 = "bgby"
-    t4 = "yryg"
-    t5 = "ppgb"
-    t6 = ""
-    t7 = ""
-    return t1,t2,t3,t4,t5,t6,t7
-
-def TestTubes2_2():     #17 turns
-    t1 = "rryb"
-    t2 = "bpry"
-    t3 = "rypg"
-    t4 = "bpgb"
-    t5 = "yggp"
-    t6 = ""
-    t7 = ""
-    return t1,t2,t3,t4,t5,t6,t7
-
-def TestTubes3_1():     #23 turns
-    t1 = "ggpr"
-    t2 = "llgw"
-    t3 = "byyw"
-    t4 = "prlg"
-    t5 = "lwrw"
-    t6 = "bpby"
-    t7 = "ybpr"
-    t8 = ""
-    t9 = ""
-    return t1,t2,t3,t4,t5,t6,t7,t8,t9
-
-def TestTubes4_1():     #42 turns
-    t1 = "dayw"
-    t2 = "aygb"
-    t3 = "bpgr"
-    t4 = "rypg"
-    t5 = "arop"
-    t6 = "dwwo"
-    t7 = "bdol"
-    t8 = "olap"
-    t9 = "lgdl"
-    t10 = "rbyw"
-    t11 = ""
-    t12 = ""
-    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12
-
-def TestTubes4_2():     #39 turns
-    t1 = "romy"
-    t2 = "gygg"
-    t3 = "allo"
-    t4 = "rylb"
-    t5 = "pmpa"
-    t6 = "wmrl"
-    t7 = "mbpo"
-    t8 = "owap"
-    t9 = "ywrw"
-    t10 = "bbga"
-    t11 = ""
-    t12 = ""
-    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12
-
-def TestTubes5_1():     #70 turns
-    t1 = "lmpm"
-    t2 = "lggm"
-    t3 = "xpbe"
-    t4 = "aorl"
-    t5 = "zdwb"
-    t6 = "rboy"
-    t7 = "dwao"
-    t8 = "otpz"
-    t9 = "zbwx"
-    t10 = "xyyp"
-    t11 = "ltda"
-    t12 = "edgy"
-    t13 = "meae"
-    t14 = "zxtr"
-    t15 = "rwgt"
-    t16 = ""
-    t17 = ""
-    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17
 
 def CheckValidityTubes(tubes):
     nrBalls = {}
@@ -190,6 +100,96 @@ def main():
         moves.append(move)
         ShowSolution.FindNextUnsortedStatus(unsortedStat,move)
     print(moves)
+
+def TestTubes1_1():     #10 turns
+    t1 = "ryby"
+    t2 = "rrbb"
+    t3 = "bryy"
+    t4 = ""
+    t5 = ""
+    return t1,t2,t3,t4,t5
+
+def TestTubes2_1():     #17 turns
+    t1 = "grrb"
+    t2 = "rypp"
+    t3 = "bgby"
+    t4 = "yryg"
+    t5 = "ppgb"
+    t6 = ""
+    t7 = ""
+    return t1,t2,t3,t4,t5,t6,t7
+
+def TestTubes2_2():     #17 turns
+    t1 = "rryb"
+    t2 = "bpry"
+    t3 = "rypg"
+    t4 = "bpgb"
+    t5 = "yggp"
+    t6 = ""
+    t7 = ""
+    return t1,t2,t3,t4,t5,t6,t7
+
+def TestTubes3_1():     #23 turns
+    t1 = "ggpr"
+    t2 = "llgw"
+    t3 = "byyw"
+    t4 = "prlg"
+    t5 = "lwrw"
+    t6 = "bpby"
+    t7 = "ybpr"
+    t8 = ""
+    t9 = ""
+    return t1,t2,t3,t4,t5,t6,t7,t8,t9
+
+def TestTubes4_1():     #42 turns
+    t1 = "dayw"
+    t2 = "aygb"
+    t3 = "bpgr"
+    t4 = "rypg"
+    t5 = "arop"
+    t6 = "dwwo"
+    t7 = "bdol"
+    t8 = "olap"
+    t9 = "lgdl"
+    t10 = "rbyw"
+    t11 = ""
+    t12 = ""
+    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12
+
+def TestTubes4_2():     #39 turns
+    t1 = "romy"
+    t2 = "gygg"
+    t3 = "allo"
+    t4 = "rylb"
+    t5 = "pmpa"
+    t6 = "wmrl"
+    t7 = "mbpo"
+    t8 = "owap"
+    t9 = "ywrw"
+    t10 = "bbga"
+    t11 = ""
+    t12 = ""
+    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12
+
+def TestTubes5_1():     #70 turns
+    t1 = "lmpm"
+    t2 = "lggm"
+    t3 = "xpbe"
+    t4 = "aorl"
+    t5 = "zdwb"
+    t6 = "rboy"
+    t7 = "dwao"
+    t8 = "otpz"
+    t9 = "zbwx"
+    t10 = "xyyp"
+    t11 = "ltda"
+    t12 = "edgy"
+    t13 = "meae"
+    t14 = "zxtr"
+    t15 = "rwgt"
+    t16 = ""
+    t17 = ""
+    return t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17
 
 if __name__ == "__main__":
     main()
